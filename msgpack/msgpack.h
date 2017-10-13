c4f9c35d6df2f5dbfe5f60e1554c3ab6784a21de
Ricardo Pereira <m@ricardopereira.eu>
2017-10-13 22:40:22 +0100

    Update public header

--- msgpack/msgpack.h
+++ msgpack/msgpack.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char msgpackVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <msgpack/PublicHeader.h>
 
-
+#import "MessagePack.h"
